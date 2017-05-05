15fffebbd823fbb6abcc1c2aac224ef517f669aa
Piotr Gogolin <loop@zamiast.com>
2017-05-05 18:54:24 +0000

    add 2 test

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -4,7 +4,21 @@ describe HomepageController do
 	describe '#index' do
 		it 'returns 200' do
 			get :index
-			expect(response.status).to be 202			
+			expect(response.status).to be 200			
+		end
+	end
+
+	describe '#about' do
+		it 'returns 200' do
+			get :about
+			expect(response.status).to be 200			
+		end
+	end
+
+	describe '#cookies' do
+		it 'returns 200' do
+			get :cookies
+			expect(response.status).to be 200			
 		end
 	end
 end
