3b474d1efe05d61cdb790db763bc4f8c314886ed
Piotr Gogolin <loop@zamiast.com>
2017-05-06 14:13:43 +0000

    modify schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20170505185152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
