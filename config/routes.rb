bb46daacb7b2b329f48c127b43bc80b0a22911fd
Piotr Gogolin <loop@zamiast.com>
2017-05-06 09:42:34 +0000

    add changes from last lesson

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
+  resources :categories
+
   get '/about', to: 'homepage#about'
 
   get '/cookies', to: 'homepage#cookies'
