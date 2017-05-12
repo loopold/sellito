1f870e9ce7868f09006aa3a67fcfffbcd2bd28fa
Piotr Gogolin <loop@zamiast.com>
2017-05-12 16:00:13 +0000

    refactor CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :fetch_category, only: [:show, :edit, :update, :destroy]
 
   def index
   	# binding.pry
@@ -30,28 +31,26 @@ class CategoriesController < ApplicationController
 
   # metody ktore nic nie robia w jednej linijce
   # def show; end
-  def show
-  	@category = Category.find(params[:id])
-  end
+  def show; end
 
-  def edit
-  	@category = Category.find(params[:id])
-  end
+  def edit; end
 
   def update
-  	@category = Category.find(params[:id])
-  	@category.update_attributes(category_params)
-  	redirect_to @category
+    @category.update_attributes(category_params)
+    redirect_to @category
   end
 
   def destroy
-  	@category = Category.find(params[:id])
-  	@category.destroy!
-  	flash[:notice] = "Category #{@category.name} deleted"
-  	redirect_to categories_path
+    @category.destroy!
+    flash[:notice] = "Category #{@category.name} deleted"
+    redirect_to categories_path
   end
 
   private
+
+  def fetch_category
+  	@category = Category.find(params[:id])
+  end
 
   def category_params
   	# tylko zapisze name
