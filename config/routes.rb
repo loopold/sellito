f1568ec58652e96d2f0e7b6e51fa07d707ad48f3
Piotr Gogolin <loop@zamiast.com>
2017-05-13 10:25:40 +0000

    add my_posts

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   get '/cookies', to: 'homepage#cookies'
 
+  get '/my_posts', to: 'posts#user_posts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
