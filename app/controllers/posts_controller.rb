53bbf4ad1e94c49bb5d2676d83d70991a9db5fdd
Piotr Gogolin <loop@zamiast.com>
2017-05-13 07:57:41 +0000

    add edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :fetch_post, only: [:show, :edit, :update, :destroy]
 
   def index
     # @posts = Post.all
@@ -28,7 +29,22 @@ class PostsController < ApplicationController
 
   def edit; end
 
+  def update
+    @post.update_attributes(post_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post.destroy!
+    flash[:notice] = "Post #{@post.title} deleted"
+    redirect_to posts_path
+  end
+
   private
+
+  def fetch_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :description, :expiration_date, :user_id)
