314eb312091a1e17cbca8a327b20894f17dc3768
Piotr Gogolin <loop@zamiast.com>
2017-05-20 19:54:43 +0000

    add seed file with users and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'admin@localhost', password: 'password', is_admin?: true)
+User.create(email: 'loop@zamiast.net', password: 'password', is_admin?: false)
+
+categories = %w(home fashion garden beauty kids edu food moto it health hobby sport pets music sex other)
+categories.each { |category| Category.create(name: category) }
